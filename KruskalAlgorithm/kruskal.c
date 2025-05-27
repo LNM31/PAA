adb6e9f6b54a4653351aad73f6b980501826ba82
LNM31 <licanemes31@gmail.com>
2025-05-27 21:09:52 +0300

    .

--- KruskalAlgorithm/kruskal.c
+++ KruskalAlgorithm/kruskal.c
@@ -5,98 +5,98 @@
 #define N 50
 
 int edge[N][3];
-
 int citire(const char* file_name)
 {
-    FILE* f = fopen(file_name, "r");
-    if (f == NULL)
-    {
-        perror("Error opening file!");
-        exit(-1);
-    }
-    int n = 0; //nr de noduri ,irelevant in acest caz
-    if (fscanf(f, "%d", &n) != 1)
-    {
-        perror(NULL);
-        exit(-1);
-    }
-    int count = 0; //nr_muchii
-    int a, b, c;
-    while (fscanf(f, "%d %d %d", &a, &b, &c) == 3)
-    {
-        edge[count][0] = a;
-        edge[count][1] = b;
-        edge[count++][2] = c;
-    }
-    if (fclose(f) != 0)
-    {
-        perror("Error closing file!");
-        exit(-1);
-    }
-    return count;
+	FILE* f = fopen(file_name, "r");
+	if (f == NULL)
+	{
+		perror(NULL);
+		exit(-1);
+	}
+	int count = 0, n = 0;
+	if (fscanf(f, "%d", &n) != 1)
+	{
+		perror(NULL);
+		exit(-1);
+	}
+	int a, b, c;
+	while (fscanf(f, "%d %d %d", &a, &b, &c) == 3)
+	{
+		edge[count][0] = a;
+		edge[count][1] = b;
+		edge[count++][2] = c;
+	}
+	if (fclose(f) != 0)
+	{
+		perror(NULL);
+		exit(-1);
+	}
+	return count;
 }
-
 int comparator(const int p1[], const int p2[])
 {
-    return p1[2] - p2[2];
+	return p1[2] - p2[2];
 }
-
-int findParent(int parent[], int component)
+int findParent(int parinte[], int componenta)
 {
-    if (parent[component] == component)
-        return component;
+	if (parinte[componenta] == componenta)
+		return componenta;
 
-    return parent[component] = findParent(parent, parent[component]);
+	return parinte[componenta] = findParent(parinte, parinte[componenta]);
 }
-
-void unionSet(int u, int v, int parent[], int rank[])
+void unionSet(int v, int u, int parinte[], int rank[])
 {
-    u = findParent(parent, u);
-    v = findParent(parent, v);
+	u = findParent(parinte, u);
+	v = findParent(parinte, v);
 
-    if (rank[u] < rank[v]) 
-        parent[u] = v;
-    else if (rank[u] > rank[v])
-        parent[v] = u;
-    else 
-    {
-        parent[v] = u;
-        rank[u]++;
-    }
+	if (rank[u] < rank[v])
+		parinte[u] = v;
+	else if (rank[u] > rank[v])
+		parinte[v] = u;
+	else
+	{
+		parinte[v] = u;
+		rank[u]++;
+	}
 }
-
-int kruskal(int size, int edge[N][3])
+void kruskal(int edge[N][3], int size)
 {
-    qsort(edge, size, sizeof(edge[0]), comparator);
-    int parent[N];
-    int rank[N];
+	qsort(edge, size, sizeof(edge[0]), comparator);
+	int parinte[N];
+	int rank[N];
 
-    //MakeSet
-    for (int i = 0; i < size; i++) 
-    {
-        parent[i] = i;
-        rank[i] = 0;
-    }
+	for (int i = 0; i < size; i++)
+	{
+		parinte[i] = i;
+		rank[i] = 0;
+	}
 
-    int minCost = 0;
-    for (int i = 0; i < size; i++) 
-    {
-        int v1 = findParent(parent, edge[i][0]);
-        int v2 = findParent(parent, edge[i][1]);
-        if (v1 != v2) {
-            unionSet(v1, v2, parent, rank);
-            minCost += edge[i][2];
-            printf("%d - %d : %d\n", edge[i][0], edge[i][1], edge[i][2]);
-        }
-    }
-
-    return minCost;
+	int minCost = 0;
+	for (int i = 0; i < size; i++)
+	{
+		int v1 = findParent(parinte, edge[i][0]);
+		int v2 = findParent(parinte, edge[i][1]);
+		if (v1 != v2)
+		{
+			unionSet(v1, v2, parinte, rank);
+			printf("%d - %d  %d\n", edge[i][0], edge[i][1], edge[i][2]);
+			minCost += edge[i][2];
+		}
+	}
+	printf("\nminCost: %d \n", minCost);
 }
 
-int main()
+int main(int argc,char **argv)
 {
-    int n = citire("in.txt");
-    printf("\nCost minim: %d\n", kruskal(14, edge));
-
-    return 0;
+	if (argc != 2)
+	{
+		perror(NULL);
+		exit(-1);
+	}
+	int n = citire(argv[1]);
+	kruskal(edge, n);
+	return 0;
 }
+
+
+
--- PrimAlgorithm/prim.c
+++ PrimAlgorithm/prim.c
@@ -1,96 +1,104 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include<stdio.h>
-#include<stdbool.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-#define N 20 
+#define N 30
+#define inf 99999
 
-int graph[N][N];
+int matrice[N][N];
 
 int citire(const char* file_name)
 {
-    FILE* f = fopen(file_name, "r");
-    if (f == NULL)
-    {
-        perror("Error opening file!");
-        exit(-1);
-    }
-    int n = 0;
-    if (fscanf(f, "%d", &n) != 1)
-    {
-        perror(NULL);
-        exit(-1);
-    }
-    int a, b, c;
-    while (fscanf(f, "%d %d %d", &a, &b, &c) == 3)
-    {
-        graph[a % n][b % n] = c;
-        graph[b % n][a % n] = c;
-    }
-    if (fclose(f) != 0)
-    {
-        perror("Error closing file!");
-        exit(-1);
-    }
-    return n;
+	FILE *f = fopen(file_name, "r");
+	if (f == NULL)
+	{
+		perror(NULL);
+		exit(-1);
+	}
+	int n = 0;
+	if (fscanf(f, "%d", &n) != 1)
+	{
+		perror(NULL);
+		exit(-1);
+	}
+	int a, b, c;
+	while (fscanf(f,"%d %d %d", &a, &b, &c) == 3)
+	{
+		matrice[a % n][b % n] = c;
+		matrice[b % n][a % n] = c;
+	}
+	if (fclose(f) != 0)
+	{
+		perror(NULL);
+		exit(-1);
+	}
+	return n;
 }
-int minKey(int key[], bool set[],int size)
-{
-    int minim = INT_MAX, minim_index=-1;
-    for (int i = 0; i < size; i++)
-    {
-        if (set[i] == false && key[i] < minim)
-        {
-            minim = key[i];
-            minim_index = i;
-        }
-    }
-    return minim_index;
-}
-void display(int parinte[N], int graph[N][N],int size)
-{
-    printf("muchie  val\n");
-    int val = 0;
-    for (int i = 1; i < size; i++)
-    {
-        printf("%d - %d    %d\n", parinte[i], i,graph[parinte[i]][i]);
-        val += graph[parinte[i]][i];
-    }
-    printf("\nMinCost: %d\n",val);
-}
-void prim(int graph[N][N],int size)
-{
-    int parinte[N];
-    int key[N];
-    bool set[N];
 
-    for (int i = 0; i < size; i++)
-    {
-        key[i] = INT_MAX;
-        set[i] = false;
-    }
-
-    key[0] = 0;
-    parinte[0] = -1;
-
-    for (int i = 0; i < size - 1; i++)
-    {
-        int u = minKey(key, set,size);
-        set[u] = true;
-        for (int v = 0; v < size; v++)
-        {
-            if (graph[u][v] && set[v] == false && graph[u][v] < key[v])
-            {
-                key[v] = graph[u][v];
-                parinte[v] = u;
-            }
-        }
-    }
-    display(parinte,graph,size);
-}
-int main()
+int minKey(int cheie[N], int vizitat[N], int size)
 {
-    int n = citire("in.txt");
-    prim(graph,n);
-    return 0;
+	int minim = inf, min_index = 0;
+	for (int i = 0; i < size; i++)
+	{
+		if (vizitat[i] == 0 && cheie[i] < minim)
+		{
+			minim = cheie[i];
+			min_index = i;
+		}
+	}
+	return min_index;
+}
+
+void display(int matrice[N][N],int parinte[N], int size)
+{
+	int val = 0;
+	printf("muchie  val\n");
+	for (int i = 1; i < size; i++)
+	{
+		printf("%d - %d   %d\n", parinte[i], i, matrice[parinte[i]][i]);
+		val += matrice[parinte[i]][i];
+	}
+	printf("\nval: %d\n", val);
+}
+
+void prim(int matrice[N][N], int size)
+{
+	int cheie[N];
+	int parinte[N];
+	int vizitat[N];
+
+	for (int i = 0; i < size; i++)
+	{
+		cheie[i] = inf;
+		vizitat[i] = 0;
+	}
+
+	cheie[0] = 0;
+	parinte[0] = -1;
+
+	for (int i = 0; i < size - 1; i++)
+	{
+		int u = minKey(cheie, vizitat, size);
+		vizitat[u] = 1;
+		for (int v = 0; v < size; v++)
+		{
+			if (matrice[u][v] && vizitat[v] == 0 && matrice[u][v] < cheie[v])
+			{
+				cheie[v] = matrice[u][v];
+				parinte[v] = u;
+			}
+		}
+	}
+	display(matrice, parinte, size);
+}
+int main(int argc, char** argv)
+{
+	if (argc != 2)
+	{
+		perror("Error number of arguments");
+		exit(-1);
+	}
+	int n = citire(argv[1]);
+	prim(matrice, n);
+	return 0;
 }
